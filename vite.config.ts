a52fb3727539e3ee63770f8ff80638e573ad98bb
Amalia Pita da Veiga <amalia.pita@vml.com>
2025-03-29 18:40:32 +0100

    update vite and package

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite' // Importa loadEnv
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => { // Recibe el 'mode'
+  const env = loadEnv(mode, process.cwd(), '') // Carga las variables de entorno
 
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  return {
+    base: env.NODE_ENV === 'production' ? '/sofa-ar-project/' : '/', // Lógica condicional
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+  }
 })
